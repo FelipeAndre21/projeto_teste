5d303a3e8e8acc76edda6ba87c1fe5de0864bceb
FelipeAndre21 <felipeandre-21@hotmail.com>
2020-08-21 19:38:20 -0300

    Criando classe conta

---
+++ conta.py
@@ -0,0 +1,23 @@
+class Conta:
+
+    def __init__(self, nome, sobrenome, saldo_inicial, senha, numero_conta):
+        self.nome = nome
+        self.sobrenome = sobrenome
+        self.saldo_inicial = saldo_inicial
+        self.senha = senha
+        self.numero_conta = numero_conta
+
+    def validar_senha(self):
+        pass
+
+    def editar_conta(self):
+        pass
+
+    def deletar_conta(self):
+        pass
+
+    def mudar_saldo(self,):
+        pass
+
+    def sair(self):
+        pass
