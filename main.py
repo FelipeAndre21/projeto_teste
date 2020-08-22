c162b148da51883ba54d06059ece9220af8f0491
FelipeAndre21 <felipeandre-21@hotmail.com>
2020-08-22 01:05:19 -0300

    inserindo o metodo criar conta

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-print("******* Digite a opção desejada *******")
+import conta
+print("******* MENU *******")
 print("(1) criar conta - (2) editar conta - (3) deletar conta - (4) mudar saldo - (5) sair")
+opcao = int(input("Escolha sua opção desejada: "))
+if opcao == 1:
+    print("criar conta")
+    conta.Conta.criar_conta()
+elif opcao ==2:
+    print("editar conta")
+elif opcao == 3:
+    print("deletar conta")
+elif opcao == 4:
+    print("mudar saldo para {}".format(conta.Conta.mudar_saldo()))
+
+elif opcao == 5:
+    print("sair")
+else:
+    print("voce digitou opção inexistente")
