08484f84b4904d51d8f5222df07e1d0d871f21ab
FelipeAndre21 <felipeandre-21@hotmail.com>
2020-09-02 20:25:15 -0300

    usando while

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-import conta
+from conta import Conta
 print("******* MENU *******")
 print("(1) criar conta - (2) editar conta - (3) deletar conta - (4) mudar saldo - (5) sair")
-opcao = int(input("Escolha sua opção desejada: "))
-if opcao == 1:
-    print("criar conta")
-    conta.Conta.criar_conta()
-elif opcao ==2:
-    print("editar conta")
-elif opcao == 3:
-    print("deletar conta")
-elif opcao == 4:
-    print("mudar saldo para {}".format(conta.Conta.mudar_saldo()))
+opcao = 1
+while opcao == 1 or 2 or 3 or 4 or 5:
+    opcao = int(input("Escolha uma opção: "))
+    if opcao == 1:
+        print("criar conta")
+        Conta.criar_conta()
+    elif opcao == 2:
+        print("editar conta")
+    elif opcao == 3:
+        print("deletar conta")
+    elif opcao == 4:
+        print("mudar saldo para")
+        saldo = float(input("Novo saldo: "))
 
-elif opcao == 5:
-    print("sair")
+    elif opcao == 5:
+        print("sair")
+        break
 else:
     print("voce digitou opção inexistente")
