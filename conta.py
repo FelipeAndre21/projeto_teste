7d279b433167da69e6700b8aaf300fb6f04ce73a
FelipeAndre21 <felipeandre-21@hotmail.com>
2020-09-26 17:43:41 -0300

    salvando dados em arquivo .txt

--- conta.py
+++ conta.py
@@ -9,13 +9,27 @@ class Conta:
         self.numero_conta = numero_conta
 
     def criar_conta():
-        nome = str(input("nome: "))
-        sobrenome = str(input("sobrenome: "))
-        saldo = float(input("seu saldo: "))
-        senha = int(input("sua senha: "))
-        numero_conta = random.randint(100000,999999)
-        print("Conta de {} {} possui saldo R$ {} seu numero da conta {}".format(nome, sobrenome, saldo, numero_conta))
+        while True:
+            nome = str(input("nome: ")).capitalize()
+            sobrenome = str(input("sobrenome: ")).capitalize()
+            saldo = float(input("seu saldo: "))
+            senha = int(input("sua senha: "))
+            numero_conta = random.randint(100000,999999)
 
+
+            arquivo = open('gerentebd.txt', 'a')
+            arquivo.write(
+                "A conta  nome {} sobrenome {} saldo R$ {:.2f} senha {} conta {}\n".format(nome, sobrenome,saldo,
+                                                                           senha, numero_conta))
+
+            arquivo.close()
+
+            arquivo = open('gerentebd.txt', 'r')
+            for linha in arquivo:
+                linha = linha.rstrip()
+                print(linha)
+            arquivo.close()
+            break;
     def validar_senha(self):
         pass
 
